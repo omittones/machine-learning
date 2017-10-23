fa94ec1c0b69b517fb7a80957a1ec89b30ad4cd1
Augustin Juricic <ajuricic@neogov.net>
2017-10-23 20:04:42 +0200

    Move files to convnet.

--- NeuralMotion.Test/Program.cs
+++ NeuralMotion.Test/Program.cs
@@ -1,89 +1,13 @@
 ﻿using System;
+using ConvNetSharp.Core.Training.Double;
 using ConvNetSharp.Core.Layers.Double;
 using ConvNetSharp.Volume.Double;
-using ConvNetSharp.Core.Training.Double;
 using ConvNetSharp.Core;
 using ConvNetSharp.Volume;
 using System.Linq;
-using ConvNetSharp.Core.Layers;
-using System.Diagnostics;
 
 namespace NeuralMotion.Test
 {
-    public class ReinforcementSoftmax : SoftmaxLayer<double>, IReinforcementLayer<double>
-    {
-        private Volume<double> lossVolume;
-        private double[] loss;
-
-        public ReinforcementSoftmax(int classCount) : base(classCount)
-        {
-        }
-
-        public void SetLoss(double[] loss)
-        {
-            this.loss = loss;
-
-            if (this.lossVolume == null ||
-               !this.lossVolume.Shape.Equals(this.InputActivationGradients.Shape))
-            {
-                this.lossVolume = BuilderInstance<double>.Volume.SameAs(
-                    this.InputActivationGradients.Storage,
-                    this.InputActivationGradients.Shape);
-            }
-
-            var count = this.lossVolume.Shape.GetDimension(3);
-            if (count != loss.Length)
-                throw new NotSupportedException("Output vs loss does not match!");
-
-            for (var n = 0; n < loss.Length; n++)
-                for (var w = 0; w < this.lossVolume.Shape.GetDimension(0); w++)
-                    for (var h = 0; h < this.lossVolume.Shape.GetDimension(1); h++)
-                        for (var d = 0; d < this.lossVolume.Shape.GetDimension(2); d++)
-                            this.lossVolume.Set(w, h, d, n, loss[n]);
-        }
-
-        public override void Backward(Volume<double> y, out double loss)
-        {
-            loss = this.loss.Sum();
-
-            this.OutputActivation.DoMultiply(this.lossVolume, this.InputActivationGradients);
-        }
-    }
-
-    public interface IReinforcementLayer<T> : ILastLayer<T>
-        where T : struct, IEquatable<T>, IFormattable
-    {
-        void SetLoss(double[] loss);
-    }
-
-    public class ReinforcementTrainer : SgdTrainer
-    {
-        private readonly IReinforcementLayer<double> lossLayer;
-
-        public ReinforcementTrainer(Net<double> net) : base(net)
-        {
-            this.lossLayer = net.Layers
-                .OfType<IReinforcementLayer<double>>()
-                .FirstOrDefault();
-        }
-
-        public void Reinforce(Volume<double> inputs, double[] loss)
-        {
-            var outputs = this.Forward(inputs);
-
-            this.lossLayer.SetLoss(loss);
-
-            this.Backward(outputs);
-
-            var batchSize = inputs.Shape.GetDimension(3);
-            var chrono = Stopwatch.StartNew();
-
-            TrainImplem();
-
-            this.UpdateWeightsTimeMs = chrono.Elapsed.TotalMilliseconds / batchSize;
-        }
-    }
-
     public static class Program
     {
         public static double[] Gradients(this Net<double> net)
@@ -111,8 +35,8 @@ namespace NeuralMotion.Test
             net.AddLayer(new FullyConnLayer(10));
             net.AddLayer(new LeakyReluLayer());
             net.AddLayer(new FullyConnLayer(4));
-            net.AddLayer(new ReinforcementSoftmax(4));
-            const int SIZE = 1;
+            net.AddLayer(new ReinforcementSoftmaxLayer(4));
+            const int SIZE = 100;
 
             var trainer = new ReinforcementTrainer(net);
             trainer.LearningRate = 0.01;
@@ -128,23 +52,7 @@ namespace NeuralMotion.Test
             {
                 var inputs = BuilderInstance.Volume.SameAs(Shape.From(2, 2, 2, SIZE));
                 inputs.Storage.MapInplace(i => rnd.Next(0, 2));
-
-                var expectedClasses = new int[SIZE];
-                var reshaped = inputs.ReShape(1, 1, 8, SIZE);
-                for (var n = 0; n < SIZE; n++)
-                {
-                    var sum = 0;
-                    for (var i = 0; i < 8; i++)
-                        sum += (int)reshaped.Get(0, 0, i, n);
-                    if (sum < 3)
-                        expectedClasses[n] = 0;
-                    else if (sum < 5)
-                        expectedClasses[n] = 1;
-                    else if (sum < 7)
-                        expectedClasses[n] = 2;
-                    else
-                        expectedClasses[n] = 3;
-                }
+                int[] expectedClasses = BuildClasses(SIZE, inputs);
 
                 var outputs = net.Forward(inputs, false);
                 var predictedClasses = net.GetPrediction();
@@ -167,6 +75,31 @@ namespace NeuralMotion.Test
 
                 Console.WriteLine($"LOSS: {trainer.Loss:0.00} ACC:{accuracy:0.00}% NMCHANGES:{nmChanges}");
             }
+        }
+
+        private static int[] BuildClasses(int SIZE, Volume<double> inputs)
+        {
+            var expectedClasses = new int[SIZE];
+            var reshaped = inputs.ReShape(1, 1, -1, SIZE);
+            for (var n = 0; n < SIZE; n++)
+            {
+                expectedClasses[n] = 0;
+                continue;
+
+                var sum = 0;
+                for (var i = 0; i < 8; i++)
+                    sum += (int)reshaped.Get(0, 0, i, n);
+                if (sum < 3)
+                    expectedClasses[n] = 0;
+                else if (sum < 5)
+                    expectedClasses[n] = 1;
+                else if (sum < 7)
+                    expectedClasses[n] = 2;
+                else
+                    expectedClasses[n] = 3;
+            }
+
+            return expectedClasses;
         }
     }
 }
